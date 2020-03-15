b5e571782db51ff81c1eb6ae1713adc3ed92152b
outerspace-probes <hello@outerspace.pl>
2020-03-15 21:21:27 +0100

    ui up

--- Assets/Scripts/StartMenuController.cs
+++ Assets/Scripts/StartMenuController.cs
@@ -2,9 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartMenuController : MonoBehaviour
 {
+    int highScore = 0;
+    public Text highScoreText;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("highScore"))
+        {
+            highScore = PlayerPrefs.GetInt("highScore");
+        }
+        highScoreText.text = highScore.ToString();
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene("Game");
