40e21fc7f036c3f15cf693e41d1db0d2d8fb9dac
outerspace-probes <hello@outerspace.pl>
2020-03-17 00:19:33 +0100

    achievements ui

--- Assets/Scripts/StartMenuController.cs
+++ Assets/Scripts/StartMenuController.cs
@@ -8,6 +8,10 @@ public class StartMenuController : MonoBehaviour
 {
     int highScore = 0;
     public Text highScoreText;
+    [SerializeField] GameObject achievKoalasIconLock;
+    [SerializeField] GameObject achievKoalasIconChecked;
+    [SerializeField] GameObject achievDistanceIconLock;
+    [SerializeField] GameObject achievDistanceIconChecked;
 
     private void Start()
     {
@@ -16,6 +20,24 @@ public class StartMenuController : MonoBehaviour
             highScore = PlayerPrefs.GetInt("highScore");
         }
         highScoreText.text = highScore.ToString();
+
+        if(PlayerPrefs.HasKey("AchievKoalasDone") && PlayerPrefs.GetInt("AchievKoalasDone") == 1)
+        {
+            achievKoalasIconChecked.SetActive(true);
+        }
+        else
+        {
+            achievKoalasIconLock.SetActive(true);
+        }
+
+        if (PlayerPrefs.HasKey("AchievDistanceDone") && PlayerPrefs.GetInt("AchievDistanceDone") == 1)
+        {
+            achievDistanceIconChecked.SetActive(true);
+        }
+        else
+        {
+            achievDistanceIconLock.SetActive(true);
+        }
     }
 
     public void PlayGame()
